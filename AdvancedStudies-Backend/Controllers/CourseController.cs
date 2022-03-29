48b5c14f9c5f974fc089011bf6857620c54606a0
Theodor Nordkvist <teddin2@outlook.com>
2022-03-29 22:19:19 +0200

    Add authorization to GET method

--- AdvancedStudies-Backend/Controllers/CourseController.cs
+++ AdvancedStudies-Backend/Controllers/CourseController.cs
@@ -1,6 +1,7 @@
 ﻿using AdvancedStudies_Backend.Data;
 using AdvancedStudies_Backend.Models.Domain;
 using AdvancedStudies_Backend.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,7 +18,7 @@ namespace AdvancedStudies_Backend.Controllers
         {
             _context = context;
         }
-
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<List<Course>>> GetCourses()
         {
